d9af585380bd8d10220dd67c19da32d02b363f45
Avinash kumar <avinash_ak@outlook.com>
2025-09-26 12:21:12 +0530

    added palindromecheck program

--- demo.java
+++ demo.java
@@ -1,1 +1,23 @@
-Hii version 1
+//palindrome check of string
+class Main {
+    public static void main(String[] args) {
+        String str = "mAdam";
+        boolean status = isPalindrome(str);
+        System.out.println("the string is palindrome: " + status);
+    }
+    public static boolean isPalindrome(String s){
+        s = s.toLowerCase();
+        String rev = "";
+        char[] ch = s.toCharArray();
+        int i = s.length() - 1;
+        while(i>=0){
+            rev += ch[i];
+            i--;
+        }
+        System.out.println("reverse: " + rev);
+        if(s.equals(rev)){
+            return true;
+        }
+        return false;
+    }
+}
